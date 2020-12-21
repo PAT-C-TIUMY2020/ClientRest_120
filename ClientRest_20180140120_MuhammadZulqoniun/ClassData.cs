e6382d4a357e09dd30b610a7b547bcf1f0b38313
Zulqoniun <muhammadqoni123@gmail.com>
2020-12-21 20:05:29 +0700

    Add project files.

---
+++ ClientRest_20180140120_MuhammadZulqoniun/ClassData.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace ClientRest_20180140120_MuhammadZulqoniun
+{
+    class ClassData
+    {
+        public void getData()
+        {
+            var json = new WebClient().DownloadString("http://localhost:1907/Mahasiswa");
+            var data = JsonConvert.DeserializeObject<List<Mahasiswa>>(json);
+
+            foreach (var mhs in data)
+            {
+                Console.WriteLine("Nama: " + mhs.nama);
+                Console.WriteLine("NIM: " + mhs.nim);
+                Console.WriteLine("Prodi: " + mhs.prodi);
+                Console.WriteLine("Angkatan: " + mhs.angkatan);
+            }
+            Console.ReadLine();
+        }
+    }
+}
